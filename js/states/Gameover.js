6082e636a87ac39ebc49e08f1a428a1f3493f553
Maxime AILLOUD <maxime.ailloud@gmail.com>
2014-05-11 19:21:50 +0200

    Write something in gameover scene

--- js/states/Gameover.js
+++ js/states/Gameover.js
@@ -2,5 +2,13 @@ FlappyPlane.Gameover = function(game) {
 };
 
 FlappyPlane.Gameover.prototype.create = function() {
-    console.log('gameover');
+    var text  = "You lose, click to restart";
+    var style = { font: "22px Arial", fill: "white", align: "center" };
+    this.add.text(this.world.centerX - 50, this.world.centerY - 20, text, style);
+
+    this.input.onDown.addOnce(this.restartGame, this);
+};
+
+FlappyPlane.Gameover.prototype.restartGame = function() {
+    this.game.state.start('Game');
 };
